1966a2e8fe5391bcd49964ea4f2308191bbfc9e9
Denis Zadniprovskiy <dzadniprovskiy@blueorigin.com>
2021-04-23 17:17:33 -0700

    Update keymap

--- keyboards/lily58/keymaps/dzadnip/keymap.c
+++ keyboards/lily58/keymaps/dzadnip/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |------+------+------+------+------+------|  [ {  |    |  ] }  |------+------+------+------+------+------|
     * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  | , >  | . >  | / ?  |RShift|
     * `-----------------------------------------/       /     \      \-----------------------------------------'
-    *                   | LGUI | LAlt |Space | / LCTRL /       \ Enter\  |Space |Layer1|  \ | |
+    *                   | LGUI | LAlt |Space | / LCTRL /       \ Enter\  |Space |Layer1|  = + |
     *                   |      |      |      |/       /         \      \ |      |      |      |
     *                   `----------------------------'           '------''--------------------'
     */
@@ -25,28 +25,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,       KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_MINS,
     KC_ESC,       KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_LSFT,      KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LBRC,        KC_RBRC,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
-                                          KC_LGUI,    KC_LALT,    KC_SPC,     KC_LCTL,        KC_ENT,     KC_SPC,     MO(1),      KC_BSLS
+                                          KC_LGUI,    KC_LALT,    KC_SPC,     KC_LCTL,        KC_ENT,     KC_SPC,     MO(1),      KC_EQL
     ),
     /* 1
     * ,-----------------------------------------.                    ,-----------------------------------------.
-    * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |      |      |      |      |      | DEL  |
+    * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | DEL  |
     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * |  F7  |  F8  |  F9  | F10  | F11  | F12  |                    |      |      |      |      |      |  = + |
+    * |      | F11  | F12  | PgUp |      |      |                    |      |      |      |  Up  |      |      |
     * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
-    * | Caps |Insert|      | Home | PgUp |      |-------.    ,-------|      |      |      |  Up  |      |      |
+    * | Caps |      | Home |PgDown| End  |      |-------.    ,-------|      |      | Left | Down |Right |      |
     * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
-    * |LShift|      |      | End  |PgDown|      |-------|    |-------|      |      | Left | Down |Right |RShift|
+    * |LShift|      |      |      |Insert|      |-------|    |-------|      |      |      |      |  \ | |RShift|
     * `-----------------------------------------/       /     \      \-----------------------------------------'
     *                   |      |      |      | /       /       \      \  |      | Trsn |      |
     *                   |      |      |      |/       /         \      \ |      |      |      |
     *                   `----------------------------'           '------''--------------------'
     */
     [1] = LAYOUT( \
-    KC_F1,        KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                                  KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_DEL,
-    KC_F7,        KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,                                 KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_EQL,
-    KC_CAPS,      KC_INS,     KC_NO,      KC_HOME,    KC_PGUP,    KC_NO,                                  KC_NO,      KC_NO,      KC_NO,      KC_UP,      KC_NO,      KC_NO,
-    KC_LSFT,      KC_NO,      KC_NO,      KC_END,     KC_PGDN,    KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_NO,      KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_RSFT,
-                                          KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_TRNS,    KC_NO
+    KC_NO,        KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                                  KC_F6,      KC_F7,        KC_F8,      KC_F9,      KC_F10,     KC_DEL,
+    KC_NO,        KC_F11,     KC_F12,     KC_PGUP,    KC_NO,      KC_NO,                                  KC_NO,      KC_NO,        KC_NO,      KC_UP,      KC_NO,      KC_NO,
+    KC_CAPS,      KC_NO,      KC_HOME,    KC_PGDN,    KC_END,     KC_NO,                                  KC_NO,      KC_NO,        KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_NO,
+    KC_LSFT,      KC_NO,      KC_NO,      KC_NO,      KC_INS,     KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_NO,        KC_NO,      KC_NO,      KC_BSLS,    KC_RSFT,
+                                          KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_TRNS,      KC_NO
     ),
 };
 
